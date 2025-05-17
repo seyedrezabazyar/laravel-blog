f3fc32fe3b8ab9e77a5b44b8e08626661f76b748
seyedrezabazyar <seyedrezabazyar@gmail.com>
2025-05-17 16:45:24 +0330

    fix author page display issue

--- resources/views/blog/author.blade.php
+++ resources/views/blog/author.blade.php
@@ -3,7 +3,6 @@
     <div class="bg-gradient-to-r from-indigo-50 to-purple-50 py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
             <h1 class="text-3xl font-bold text-gray-900 mb-2">{{ $author->name }}</h1>
-            <p class="text-gray-600">{{ $author->total_posts_count }} کتاب</p>
         </div>
     </div>
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
@@ -43,24 +42,6 @@
                 @else
                     <p class="text-gray-500 italic mb-6">اطلاعات بیوگرافی ثبت نشده است.</p>
                 @endif
-                <div class="flex items-center text-sm text-gray-500 mt-4">
-                    @if($author->posts_count > 0)
-                        <div class="flex items-center ml-6">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1 text-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/>
-                            </svg>
-                            <span>{{ $author->posts_count }} کتاب اصلی</span>
-                        </div>
-                    @endif
-                    @if($author->coauthored_count > 0)
-                        <div class="flex items-center ml-6">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1 text-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
-                            </svg>
-                            <span>{{ $author->coauthored_count }} کتاب همکاری</span>
-                        </div>
-                    @endif
-                </div>
             </div>
         </div>
         <div>
